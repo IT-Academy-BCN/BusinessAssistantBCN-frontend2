da498fbcdf8572d8242c03e56733c43f442caf61
Ramon <rferrergrau@gmail.com>
2022-05-05 12:38:54 +0200

    ngx-translate

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,29 @@
 // ANGULAR CORE & COMMON
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-// MODULE: APP ROUTING
-import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
 
 // APP COMPONENT
 import { AppComponent } from './app.component';
 
+// MODULE: APP ROUTING
+import { AppRoutingModule } from './app-routing.module';
+
 // MODULE: BROWSER ANIMATIONS MODULE, is necessary for Material 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+// @NGX-TRANSLATE/CORE
+import { TranslateLoader, TranslateModule, TranslateService } from '@ngx-translate/core';
+
+// BABCN-TRANSLATE-MODULE: TranslateHttpLoader
+import { HttpLoaderFactory } from './shared/babcn-translate.module';
+
+// MODULE: SHARED
+import { SharedModule } from './shared/shared.module';
+
 // MODULE: [FEATURE] VIRTUAL-ASSISTANT
 import { VirtualAssistantModule } from './features/virtual-assistant/virtual-assistant.module';
+
 
 @NgModule({
   declarations: [
@@ -22,10 +33,28 @@ import { VirtualAssistantModule } from './features/virtual-assistant/virtual-ass
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    HttpClientModule,
 
+
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient]
+      }
+    }),
+
+    // SHARED MODULE
+    SharedModule,
+
+    // FEATURES MODULES
     VirtualAssistantModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  exports: [
+    TranslateModule
+  ]
 })
 export class AppModule { }
+
