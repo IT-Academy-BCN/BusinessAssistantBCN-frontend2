920584d7e161b2fe4def63f260eaaab215a4a861
HenryAr <henryarturorivera@gmail.com>
2022-05-05 22:52:59 +0200

    headers bug fixed and routes created #8

--- src/app/core/components/header/header.component.ts
+++ src/app/core/components/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -27,13 +28,48 @@ export class HeaderComponent implements OnInit {
       'EN',
     ],
     end: [
-      'Login'
+      'Login',
     ]
   }
+
   
-  constructor() { }
+  constructor(private route: Router) { }
 
   ngOnInit(): void {
   }
+  
+  menuRoute(i: number){
+    if(i == 0){
+      this.route.navigate(['/les-meves-cerques'])
+    }else if(i == 1){
+      this.route.navigate(['/directori'])
+    }else if(i ==2){
+      this.route.navigate(['/qui-som'])
+    }else if(i == 3){
+      this.route.navigate(['/faqs'])
+    }else if(i == 4){
+      this.route.navigate(['/legals'])
+    }else if(i == 5){
+      this.route.navigate(['/login'])
+    }
+  }
+
+  leftButtonsRoute(i: number){
+    if(i == 0){
+      this.route.navigate(['/qui-som'])
+    }else if(i == 1){
+      this.route.navigate(['/faqs'])
+    }else if(i == 2){
+      this.route.navigate(['/legals'])
+    }
+  }
+
+  rightButtonsRoute(i: number){
+    if(i == 0){
+      this.route.navigate(['/login'])
+    }
+  }
+
+
 
 }
--- src/app/core/components/header/header.module.ts
+++ src/app/core/components/header/header.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 import { HeaderComponent } from './header.component';
 //Material
@@ -21,6 +22,7 @@ import {MatToolbarModule} from '@angular/material/toolbar';
   ],
   imports: [
     CommonModule,
+    RouterModule,
     //Material
     MatMenuModule,
     MatIconModule,
