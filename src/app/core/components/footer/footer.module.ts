f6272d272797e28a61d9d09a832a0eef447c75fc
Liss-11 <frolova.alissia@gmail.com>
2022-05-24 12:13:08 +0200

    foldering_change

--- src/app/core/components/footer/footer.module.ts
+++ src/app/core/components/footer/footer.module.ts
@@ -1,7 +1,7 @@
 
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FooterComponent } from './component/footer.component';
+import { FooterComponent } from './footer.component';
 
 // CDK
 import { LayoutModule } from '@angular/cdk/layout';
--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -4,14 +4,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 //COMPONENT MODULOS
-import { FooterModule } from '././components/footer/footer.module';
+import { FooterModule } from './components/footer/footer.module';
 import { LanguageChangerModule } from './components/language-changer/language-changer.module';
 
 // COMPONENTS
 import { BreadcrumbComponent } from './components/breadcrumb/breadcrumb.component';
 import { HeaderComponent } from './components/header/header.component';
 import { MenuComponent } from './components/menu/menu.component';
-import { FooterComponent } from './components/footer/component/footer.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
