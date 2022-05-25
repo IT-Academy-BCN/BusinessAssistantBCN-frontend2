376967c3aa316bf10c1b347b63a4d886d8236464
HenryAr <henryarturorivera@gmail.com>
2022-05-25 15:52:18 +0200

    rutas añadidas al app-routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './features/home/components/home.component';
+import { MyEnvironmentPageComponent } from './features/my-environment/pages/my-environment-page/my-environment-page.component';
+import { VirtualAssistantPageComponent } from './features/virtual-assistant/page-routing/virtual-assistant-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'My-environment', component: MyEnvironmentPageComponent},
+  {path: 'My-environment-search', component: MyEnvironmentPageComponent},
+  {path: 'My-environment-result', component: MyEnvironmentPageComponent},
+  {path: 'Virtual-assistant', component: VirtualAssistantPageComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
