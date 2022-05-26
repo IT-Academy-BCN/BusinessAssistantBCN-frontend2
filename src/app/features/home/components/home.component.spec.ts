5d7bc78ecd80da86d5bde48e9ce3fdd983f252c5
HenryAr <henryarturorivera@gmail.com>
2022-05-26 13:15:56 +0200

    home component buttons and tests done

--- src/app/features/home/components/home.component.spec.ts
+++ src/app/features/home/components/home.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { TranslateFakeLoader, TranslateLoader, TranslateModule } from '@ngx-translate/core';
 
 import { HomeComponent } from './home.component';
 
@@ -8,7 +9,15 @@ describe('HomeComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ HomeComponent ]
+      declarations: [ HomeComponent ],
+      imports: [
+        TranslateModule.forRoot({
+          loader: {
+            provide: TranslateLoader,
+            useClass: TranslateFakeLoader,
+          },
+        }),
+      ]
     })
     .compileComponents();
   });
