75b0b1ce22a74c1e95c437794306cd18474bbf47
Liss-11 <frolova.alissia@gmail.com>
2022-06-22 11:33:45 +0200

    prueba_footer

--- src/app/core/layout/footer/footer.component.ts
+++ src/app/core/layout/footer/footer.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit, AfterViewInit, OnChanges } from '@angular/core';
+import { BreakpointObserver, Breakpoints} from '@angular/cdk/layout';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+
 //import { Breakpoints } from '@angular/cdk/layout';
 import { BreakpointService } from 'src/app/services/shared/breakpoint/breakpoint.service';
 import { FOOTER_MAT_GRID_LIST } from '../../../shared/components/component-constants';
@@ -8,83 +10,80 @@ import { FOOTER_MAT_GRID_LIST } from '../../../shared/components/component-const
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss']
 })
-export class FooterComponent implements OnInit, AfterViewInit {
 
-  breakpoint: number | string = 0;
-  ratio: string | number = "";
+  
+export class FooterComponent implements OnInit, OnDestroy {
 
-  constructor(private size_observer: BreakpointService) { }
+  breakpoint: number | string | "Unknown" = 2;
+  ratio: string | number = "110";
+  breakPoints:(string | number)[] | undefined = FOOTER_MAT_GRID_LIST.get("Breakpoints.XSmall");
 
-  ngOnChange() { 
-    this.size_observer.breakpoint$.subscribe((res) => {
-      console.log(res);
-      //this.breakpointChanged(res);
-      FOOTER_MAT_GRID_LIST.forEach((value, key) => {
-        if (key == res) {
-          this.breakpoint = value[0];
-          this.ratio = value[1];
-          console.log(this.breakpoint, this.ratio);
+  constructor(private responsive: BreakpointObserver) { }
+
+  ngOnInit(): void {
+
+    this.responsive.observe([
+      Breakpoints.XSmall,
+      Breakpoints.Small,
+      Breakpoints.Medium,
+      Breakpoints.Large,
+      Breakpoints.XLarge])
+     .subscribe(() =>
+        this.breakpointChanged() 
+      );
+  }
+
+  private breakpointChanged()
+  {
+
+   
+
+    /* FOOTER_MAT_GRID_LIST.forEach(key => {
+      if (this.responsive.isMatched(key.toString())) {
+        this.arr = FOOTER_MAT_GRID_LIST.get(key.toString());
+        console.log(this.arr);
+   
+        
+        const currentScreenSize: (string | number)[] | "Unknown" = FOOTER_MAT_GRID_LIST.get(key.toString()) ?? 'Unknown';
+        this.breakpoint = this.arr[0] ?? 'Unknown';
+        this.ratio = this.arr[1]; 
+          console.log(currentScreenSize);
         }
-      });
-      //this.ngOnInit();
-    });
-  };
+      }); */
 
- ngOnInit(): void {
-
-    this.size_observer.breakpoint$.subscribe((res) => {
-      console.log(res);
-      //this.breakpointChanged(res);
-      FOOTER_MAT_GRID_LIST.forEach((value, key) => {
-        if (key == res) {
-          this.breakpoint = value[0];
-          this.ratio = value[1];
-          console.log(this.breakpoint, this.ratio);
+    /* for (const query of Object.keys(this.responsive.isMatched)) {
+          if (this.responsive.isMatched(query)) {
+            const currentScreenSize: (string | number)[] | "Unknown" = FOOTER_MAT_GRID_LIST.get(query) ?? 'Unknown';
+            this.breakpoint = currentScreenSize[0];
+            this.ratio = currentScreenSize[1];
         }
-      });
-    });
-   // this.ngOnChange(); 
-    //this.ngAfterViewInit();
+      } */
     
-  }
-  
-
-  ngAfterViewInit() { 
-    this.size_observer.breakpoint$.subscribe((res) => { 
-      console.log(res);   
-      //this.breakpointChanged(res);
-      FOOTER_MAT_GRID_LIST.forEach((value, key) => {
-        if (key == res) { 
-          this.breakpoint = value[0];
-          this.ratio = value[1];
-          console.log(this.breakpoint, this.ratio);
-        }
-      });
-    })
-  }
-  ngAfterViewChecked() { 
-    this.size_observer.breakpoint$.subscribe((res) => { 
-      console.log(res);   
-      //this.breakpointChanged(res);
-      FOOTER_MAT_GRID_LIST.forEach((value, key) => {
-        if (key == res) { 
-          this.breakpoint = value[0];
-          this.ratio = value[1];
-          console.log(this.breakpoint, this.ratio);
-        }
-      });
-    })
+    if (this.responsive.isMatched(Breakpoints.XSmall)) {
+      this.breakpoint = 1;
+      this.ratio = "110px";
+     /*  this.breakPoints = FOOTER_MAT_GRID_LIST.get("Breakpoints.XSmall") || undefined; */
+      console.log(this.breakPoints);
+    }
+    else if (this.responsive.isMatched(Breakpoints.Small)) {
+      this.breakpoint = 2;
+      this.ratio = "130px";
+      /* this.breakPoints = FOOTER_MAT_GRID_LIST.get("Breakpoints.Small") || undefined; */
+    }
+    else if (this.responsive.isMatched(Breakpoints.Medium)) {
+      this.breakpoint = 4;
+      this.ratio = "140px";
+      /* this.breakPoints = FOOTER_MAT_GRID_LIST.get("Breakpoints.Medium") || undefined; */
+    }
+    else if (this.responsive.isMatched(Breakpoints.Large) || this.responsive.isMatched(Breakpoints.XLarge)) {
+      this.breakpoint = 5;
+      this.ratio = "120px";
+      /* this.breakPoints = FOOTER_MAT_GRID_LIST.get("Breakpoints.Large"); */
+    }
   }
 
-
-
-  
-
-  /* private breakpointChanged(res: string) {
-    console.log("this is the: " + res);  
-  }  */
-
-  /* ngOnDestroy(): void {
-    this.size_observer.breakpoint$.;
-  } */
+  ngOnDestroy(): void {
+    this.responsive.ngOnDestroy();
+  }
 }
+  
