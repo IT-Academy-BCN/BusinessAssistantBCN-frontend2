2bc936bc7e0ebb43057fcd8256985052985f8d40
Javier <javiperez2010@yahoo.es>
2022-06-28 11:19:48 +0200

    new component

---
+++ src/app/features/virtual-assistant/pages/virtual-assistant/virtual-assistant.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-virtual-assistant',
+  templateUrl: './virtual-assistant.component.html',
+  styleUrls: ['./virtual-assistant.component.scss']
+})
+export class VirtualAssistantComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
