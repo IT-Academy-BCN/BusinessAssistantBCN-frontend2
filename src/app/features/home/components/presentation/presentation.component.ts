ca4897505e54e585ed9a4653cdab9045a97f4b95
Javier <javiperez2010@yahoo.es>
2022-06-29 07:30:11 +0200

    margin grid

--- src/app/features/home/components/presentation/presentation.component.ts
+++ src/app/features/home/components/presentation/presentation.component.ts
@@ -1,30 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-import { BreakpointService } from 'src/app/services/shared/breakpoint/breakpoint.service';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+
 @Component({
   selector: 'app-presentation',
   templateUrl: './presentation.component.html',
   styleUrls: ['./presentation.component.scss']
 })
 export class PresentationComponent implements OnInit {
-
-  // break: number = 2
-  // ratio: string = '800px'
-
-  // constructor(private responsive: BreakpointService) { }
-
-  // ngOnInit(): void {
-  //   this.responsive.breakpoint$.subscribe(result => {
-  //     if (result == 'XSmall') {
-  //       this.break = 1
-  //     } else if(result == 'Small') {
-  //       this.break = 1
-  //     }else {
-  //       this.break = 2
-  //       this.ratio = '300px'
-  //     }
-  //   })
-  // }
 
   breakpoint: number = 0;
   ratio: string = "";
@@ -53,23 +35,11 @@ export class PresentationComponent implements OnInit {
     }
     else if (this.responsive.isMatched(Breakpoints.Medium)) {
       this.breakpoint = 2;
-      this.ratio = "500px";
+      this.ratio = "600px";
     }
-    // else if (this.responsive.isMatched(Breakpoints.Medium)) {
-    //   this.breakpoint = 4;
-    //   this.ratio = "140px";
-    // }
-    // else if (this.responsive.isMatched(Breakpoints.Large) || this.responsive.isMatched(Breakpoints.XLarge)) {
-    //   this.breakpoint = 5;
-    //   this.ratio = "120px";
-    // }
   }
 
   ngOnDestroy(): void {
     this.responsive.ngOnDestroy();
   }
-
-
-
-  
 }
