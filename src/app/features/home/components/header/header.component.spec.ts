360bdeec66063f0fb6152d2a4c181ffba7410c21
MAC <MAC@MacBook-Emilio.local>
2022-10-19 09:52:34 +0200

    fix#159

--- src/app/features/home/components/header/header.component.spec.ts
+++ src/app/features/home/components/header/header.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { TranslateFakeLoader, TranslateLoader, TranslateModule } from '@ngx-translate/core';
 
 import { HeaderComponent } from './header.component';
 
@@ -8,7 +9,16 @@ describe('HeaderComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ HeaderComponent ]
+      declarations: [ HeaderComponent ],
+      imports: [
+        TranslateModule.forRoot({
+          loader: {
+            provide: TranslateLoader,
+            useClass: TranslateFakeLoader,
+          }, 
+        }),
+      ],
+
     })
     .compileComponents();
   });
