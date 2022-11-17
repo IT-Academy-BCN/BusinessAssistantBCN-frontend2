08041f31ebe4710fd7793d4343f16aa03dbb496d
lioandres <emiliolagarrigue5@gmail.com>
2022-11-17 10:21:25 +0100

    upper class use

--- src/app/features/my-environment/services/my-environment.service.ts
+++ src/app/features/my-environment/services/my-environment.service.ts
@@ -2,7 +2,9 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import {environment} from "../../../../environments/environment";
 import { Observable } from 'rxjs';
-import { MyEnvironmentSearch } from '../../../shared/models/my-environment-search/my-environment-search.model';
+import { MyEnvironmentSearch, SearchType } from '../../../shared/models/my-environment-search/my-environment-search.model';
+import { Zone } from 'src/app/shared/models/common/zone.model';
+
 
 @Injectable({
   providedIn: 'root'
@@ -14,24 +16,16 @@ export class MyEnvironmentService {
   constructor(private http: HttpClient) { }
 
 
-  getZones(): Observable<any>{
-    return this.http.get(
-      `${ environment.BACKEND_BASE_URL }${ environment.BACKEND_ZONES_URL }`,
-      {
-        headers: {
-          'Content-Type': 'application/json'
-        }
-      });
-  }
+ 
 
-  getEconomicActivities(category:MyEnvironmentSearch): Observable<any> {
+  getEconomicActivities(businessModel:SearchType): Observable<any> {
     const activityEndPoint=[
       {establishment :0, endPointActivity:environment.BACKEND_BIG_MALLS_ACTIVITIES_URL},
       {establishment :1, endPointActivity:environment.BACKEND_COMMERCIAL_GALLERIES_ACTIVITIES_URL},
       {establishment :2, endPointActivity:environment.BACKEND_LARGE_STABLISHMENTS_ACTIVITIES_URL}
     ]
 
-    let endPoint=activityEndPoint.find(item=> item.establishment==category.searchType)
+    let endPoint=activityEndPoint.find(item=> item.establishment==businessModel)
     if (endPoint==undefined) endPoint={establishment:0,endPointActivity:environment.BACKEND_BIG_MALLS_ACTIVITIES_URL}
     return this.http.get(
         `${ environment.BACKEND_BASE_URL }${endPoint.endPointActivity}`,
@@ -42,21 +36,21 @@ export class MyEnvironmentService {
         });
   }
 
-  getResults(businessModel: string){
+  getResults(businessModelSearch: MyEnvironmentSearch){
     let params = new HttpParams();
     /*    params = params.append('zones', JSON.stringify(this.selectedZones))
         params = params.append('activities', JSON.stringify(this.selectedActivities));   */
 //TODO
-    switch (businessModel){
-      case 'common.button.mall':
-        return this.http.get(`${environment.BACKEND_LARGE_ESTABLISHMENTS_FAKE_FILTERED_RESULTS}`,{params})
-      case 'common.button.gallery-market':
-        return this.http.get(`${environment.BACKEND_COMMERCIAL_GALLERIES_FAKE_FILTERED_RESULTS}`,{params})
-      case 'common.button.big-stablish':
+    switch (businessModelSearch.searchType){
+      case 0:
         return this.http.get(`${environment.BACKEND_BIG_MALLS_FAKE_FILTERED_RESULTS}`,{params})
-      case 'common.button.market-fair':
+      case 1:
+        return this.http.get(`${environment.BACKEND_COMMERCIAL_GALLERIES_FAKE_FILTERED_RESULTS}`,{params})
+      case 2:
+        return this.http.get(`${environment.BACKEND_LARGE_ESTABLISHMENTS_FAKE_FILTERED_RESULTS}`,{params})
+      case 3:
         return this.http.get(`${environment.BACKEND_MARKET_FAIRS_FAKE_FILTERED_RESULTS}`,{params})
-      case 'common.button.public-market':
+      case 4:
         return this.http.get(`${environment.BACKEND_MUNICIPAL_MARKETS_FAKE_FILTERED_RESULTS}`,{params})
       default:
         return this.http.get(`${environment.BACKEND_LARGE_ESTABLISHMENTS_FAKE_FILTERED_RESULTS}`,{params})
