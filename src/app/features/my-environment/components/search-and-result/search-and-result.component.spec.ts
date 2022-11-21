88c7c346ff4b0fce0b3e4bd24883b204c661c917
cplan85 <carlos_planchart@live.com>
2022-11-21 13:00:49 +0100

    passed unit tests

--- src/app/features/my-environment/components/search-and-result/search-and-result.component.spec.ts
+++ src/app/features/my-environment/components/search-and-result/search-and-result.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { HttpClientModule } from '@angular/common/http';
 import { SearchAndResultComponent } from './search-and-result.component';
+import { TranslateFakeLoader, TranslateLoader, TranslateModule } from '@ngx-translate/core';
 
 describe('SearchAndResultComponent', () => {
   let component: SearchAndResultComponent;
@@ -8,6 +9,12 @@ describe('SearchAndResultComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [HttpClientModule,  TranslateModule.forRoot({
+        loader: {
+          provide: TranslateLoader,
+          useClass: TranslateFakeLoader,
+        },
+      }),],
       declarations: [ SearchAndResultComponent ]
     })
     .compileComponents();
