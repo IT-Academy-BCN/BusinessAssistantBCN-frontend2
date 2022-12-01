7b67f6716f0805f82561c44808b21e0ef5d2f248
Daniel Alfranca Gil <webmastdag81@gmail.com>
2022-12-01 11:35:15 +0100

    testing methods defined

--- src/environments/environment.spec.ts
+++ src/environments/environment.spec.ts
@@ -20,7 +20,7 @@ describe('environtments endpoints', () => {
         [
             'BACKEND_ZONES_URL', /* ok */
             'BACKEND_LARGE_STABLISHMENTS_ACTIVITIES_URL', /* OK */
-          /*   'BACKEND_LARGE_STABLISHMENTS_SEARCH_URL', */ /* 400 - Bad Request */
+          /*   'BACKEND_LARGE_STABLISHMENTS_SEARCH_URL', */ /* 400 - Bad Request --- get issue */
             'BACKEND_COMMERCIAL_GALLERIES_ACTIVITIES_URL', /* OK */ 
             'BACKEND_SAVED_SEARCHES_URL',
             'BACKEND_BIG_MALLS_URL', /* OK  */
@@ -29,8 +29,27 @@ describe('environtments endpoints', () => {
 
         ].forEach(element => {
             
-            httpMock.expectOne(`http://87.106.229.175:7777${(environment as any)[element]}`)
-        });
-        
+            
+        });        
     });
+
+    it('should connect endpoints through proxy', () => {
+
+      [
+          'BACKEND_ZONES_URL', /* ok */
+          'BACKEND_LARGE_STABLISHMENTS_ACTIVITIES_URL', /* OK */
+        /*   'BACKEND_LARGE_STABLISHMENTS_SEARCH_URL', */ /* 400 - Bad Request --- get issue */
+          'BACKEND_COMMERCIAL_GALLERIES_ACTIVITIES_URL', /* OK */ 
+          'BACKEND_SAVED_SEARCHES_URL',
+          'BACKEND_BIG_MALLS_URL', /* OK  */
+          'BACKEND_BIG_MALLS_ACTIVITIES_URL', /*  OK  */
+          'BACKEND_MUNICIPAL_MARKETS', /*  OK  */
+
+      ].forEach(element => {
+          
+          
+      });        
+  });
+
+
 });
