ffaa92e002a617a27b3265461b7a549d5a3993a3
David Gomez Muns <davidmuns@yahoo.es>
2023-01-30 11:50:07 +0100

    Update saved-search.model.ts

--- src/app/shared/models/saved-search.model.ts
+++ src/app/shared/models/saved-search.model.ts
@@ -1,26 +1,28 @@
-/*import { BigMallModel } from "./big-mall.model";
-import { CommercialGalleryModel } from "./commercial-gallery.model";
-import { LargeStablishmentModel } from "./large-establishment.model";
-import { MarketFairModel } from "./marfet-fair.model";
-import { MunicipalMarketModel } from "./municipal-market.model";*/
+import { SearchItemResult } from './my-environment-search/search-item-result.model';
+// import { BigMallModel } from "./big-mall.model";
+// import { CommercialGalleryModel } from "./commercial-gallery.model";
+// import { LargeStablishmentModel } from "./large-establishment.model";
+// import { MarketFairModel } from "./marfet-fair.model";
+// import { MunicipalMarketModel } from "./municipal-market.model";
 
 export class SavedSearchesModel {
-/*    search_uuid: number;
-    user_uuid: number;
-    search_date: Date;
-    search_name: string;
-    search_detail: string;
-    search_result: LargeStablishmentModel[] | BigMallModel[] | CommercialGalleryModel[] | MarketFairModel[] | MunicipalMarketModel[]
-
-    constructor(search_uuid:number, user_uuid:number, search_name: string, search_date: Date, search_detail: string,
-      search_result: LargeStablishmentModel[] | BigMallModel[] | CommercialGalleryModel[] | MarketFairModel[] | MunicipalMarketModel[]
-    ) {
-      this.search_uuid = search_uuid;
-      this.user_uuid = user_uuid;
-      this.search_name = search_name;
-      this.search_date = search_date;
-      this.search_detail = search_detail;
-      this.search_result = search_result;
-    }*/
-
+  search_uuid: number;
+  user_uuid: number;
+  search_date: Date;
+  search_name: string;
+  search_detail: string;
+  // search_result: LargeStablishmentModel[] | BigMallModel[] | CommercialGalleryModel[] | MarketFairModel[] | MunicipalMarketModel[]
+  search_result: SearchItemResult[];
+  // constructor(search_name: string, search_detail: string,
+  constructor(search_uuid: number, user_uuid: number, search_name: string, search_detail: string, search_date: Date,
+    search_result: SearchItemResult[]
+  ) {
+    this.search_uuid = search_uuid;
+    this.user_uuid = user_uuid;
+    this.search_name = search_name;
+    this.search_date = search_date;
+    this.search_detail = search_detail;
+    this.search_result = search_result;
   }
+
+}
