21819be06bb40191076330ae599918e4f1dab160
David Gomez Muns <davidmuns@yahoo.es>
2023-01-31 21:08:12 +0100

    Update saved-search.model.ts

--- src/app/shared/models/saved-search.model.ts
+++ src/app/shared/models/saved-search.model.ts
@@ -1,9 +1,4 @@
 import { SearchItemResult } from './my-environment-search/search-item-result.model';
-// import { BigMallModel } from "./big-mall.model";
-// import { CommercialGalleryModel } from "./commercial-gallery.model";
-// import { LargeStablishmentModel } from "./large-establishment.model";
-// import { MarketFairModel } from "./marfet-fair.model";
-// import { MunicipalMarketModel } from "./municipal-market.model";
 
 export class SavedSearchesModel {
   user_uuid: number;
