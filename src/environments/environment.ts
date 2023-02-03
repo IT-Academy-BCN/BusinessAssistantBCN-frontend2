1613b32fb711e2d49f19407a5310dd818281faf4
David Gomez Muns <davidmuns@yahoo.es>
2023-02-03 13:39:01 +0100

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -17,7 +17,7 @@ export const environment = {
   BACKEND_MARKET_FAIRS_URL: '/opendata/market-fairs',
   BACKEND_MUNICIPAL_MARKETS_SEARCH_URL: '/opendata/municipal-markets/search',
   BACKEND_MARKET_FAIRS_SEARCH_URL: '/opendata/market-fairs/search', // 400 (Bad Request)
-  BACKEND_SAVED_SEARCHES_URL: 'http://localhost:8763/businessassistantbcn/api/v1/mydata/mysearches/',
+  BACKEND_SAVED_SEARCHES_URL: '/mydata/mysearches/',
   // fake urls
   FAKE_BACKEND_SAVED_SEARCHES_URL: '../../assets/dummy/saved-searches/saved-search-large-establishments_dummy.json',
   // User id
